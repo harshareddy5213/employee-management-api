ea5a1802de5159e0aa11d3b59bf5f4b13183bce3
Harsha Kumar Reddy <harshareddy5213@gmail.com>
2026-07-29 15:13:04 +0530

    Add employee CRUD REST endpoints

--- app/routers/employees.py
+++ app/routers/employees.py
@@ -1,1 +1,107 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from .. import crud, schemas
+from ..database import get_db
+
+
+router = APIRouter(
+    prefix="/employees",
+    tags=["Employees"]
+)
+
+
+@router.post(
+    "/",
+    response_model=schemas.EmployeeResponse
+)
+def create_employee(
+    employee: schemas.EmployeeCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.create_employee(db, employee)
+
+
+@router.get(
+    "/",
+    response_model=list[schemas.EmployeeResponse]
+)
+def get_employees(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db)
+):
+    return crud.get_employees(
+        db,
+        skip=skip,
+        limit=limit
+    )
+
+
+@router.get(
+    "/{employee_id}",
+    response_model=schemas.EmployeeResponse
+)
+def get_employee(
+    employee_id: int,
+    db: Session = Depends(get_db)
+):
+    employee = crud.get_employee(
+        db,
+        employee_id
+    )
+
+    if not employee:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    return employee
+
+
+@router.put(
+    "/{employee_id}",
+    response_model=schemas.EmployeeResponse
+)
+def update_employee(
+    employee_id: int,
+    employee: schemas.EmployeeCreate,
+    db: Session = Depends(get_db)
+):
+    updated_employee = crud.update_employee(
+        db,
+        employee_id,
+        employee
+    )
+
+    if not updated_employee:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    return updated_employee
+
+
+@router.delete(
+    "/{employee_id}"
+)
+def delete_employee(
+    employee_id: int,
+    db: Session = Depends(get_db)
+):
+    deleted_employee = crud.delete_employee(
+        db,
+        employee_id
+    )
+
+    if not deleted_employee:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    return {
+        "message": "Employee deleted successfully"
+    }
